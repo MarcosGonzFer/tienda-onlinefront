c4c367f166573c8647a62289b4a450d7a811f956
MarcosGonzFer <marcos0gon@gmail.com>
2024-11-19 12:18:28 +0100

    Comenzamos el login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
-import Header from './componentes/header/Header.jsx'
-import Footer from './componentes/Footer/Footer.jsx';
-import Section from './componentes/section/Section.jsx';
-import Nav from './componentes/Nav/Nav.jsx';
+import Login from './componentes/Login/Login';
+// import Header from './componentes/header/Header.jsx'
+// import Footer from './componentes/Footer/Footer.jsx';
+// import Section from './componentes/section/Section.jsx';
+// import Nav from './componentes/Nav/Nav.jsx';
 function App() {
   return (
     <div className="App">
-      <Header/>
+      {/* <Header/>
       <Nav/>
       <Section/>
-      <Footer/>
+      <Footer/> */}
+      <Login/>
     </div>
   );
 }
