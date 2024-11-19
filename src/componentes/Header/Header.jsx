6fe0c016f6e818bb34f415df9cbecbffe58d2d1a
MarcosGonzFer <marcos0gon@gmail.com>
2024-11-19 12:36:02 +0100

    rename folders

--- src/componentes/Header/Header.jsx
+++ src/componentes/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Header.css";
 
 import Logo from "../Logo/Logo";
-import Links from "../links/Links";
+import Links from "../Links/Links";
 
 function Header() {
   return (
