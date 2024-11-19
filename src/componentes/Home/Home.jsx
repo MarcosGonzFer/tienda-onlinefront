96b63212bf0797d5c2d099a7cbfbffbac16aab1e
Blasonic <168122520+Blasonic@users.noreply.github.com>
2024-11-19 12:14:42 +0100

    Cambios en el home

--- src/componentes/Home/Home.jsx
+++ src/componentes/Home/Home.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import Header from './componentes/header/Header.jsx'
-import Footer from './componentes/Footer/Footer.jsx';
-import Section from './componentes/section/Section.jsx';
-import Nav from './componentes/Nav/Nav.jsx';
+import Header from '../Header/Header.jsx';
+import Footer from '../Footer/Footer.jsx';
+import Section from '../Section/Section.jsx';
+import Nav from '../Nav/Nav.jsx';
 function Home() {
   return (
     <div>
--- src/componentes/header/Header.jsx
+++ src/componentes/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Header.css";
 
-import Logo from "../logo/Logo";
+import Logo from "../Logo/Logo";
 import Links from "../links/Links";
 
 function Header() {
