0f37fa0c427f595e7fe2d32f9df6bd861722924e
MarcosGonzFer <marcos0gon@gmail.com>
2024-11-19 13:10:54 +0100

    Añadimos el login al app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Home from './componentes/Home/Home';
+import Login from './componentes/Login/Login';
 
 function App() {
   return (
     <div className="App">
       <Home/>
+      <Login/>
     </div>
   );
 }
