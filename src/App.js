443ce181596d4833294732d3ace10d39e7400583
MarcosGonzFer <marcos0gon@gmail.com>
2024-11-19 12:33:08 +0100

    cambios en app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import './App.css';
 import Login from './componentes/Login/Login';
-// import Header from './componentes/header/Header.jsx'
-// import Footer from './componentes/Footer/Footer.jsx';
-// import Section from './componentes/section/Section.jsx';
-// import Nav from './componentes/Nav/Nav.jsx';
+
 function App() {
   return (
     <div className="App">
-      {/* <Header/>
-      <Nav/>
-      <Section/>
-      <Footer/> */}
+
       <Login/>
     </div>
   );
