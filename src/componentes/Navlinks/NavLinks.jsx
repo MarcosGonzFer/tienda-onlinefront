32b4ea5b1e93a85374c105a491fee65e2cc58e5c
jaime gomez <jagoalius@gmail.com>
2024-11-26 09:42:33 +0100

    Cambios de link

--- src/componentes/Navlinks/NavLinks.jsx
+++ src/componentes/Navlinks/NavLinks.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import './NavLinks.css'
-import NavBuscador from '../NavBuscador/NavBuscador'
-
+import React from 'react';
+import './NavLinks.css';
+import NavBuscador from '../NavBuscador/NavBuscador';
+import { Link } from 'react-router-dom'; 
 
 function NavLinks() {
   return (
     <div className='contenedor-nav'>
-    <a href="#">Marcas</a>
-    <a href="#">Hombres</a>
-    <a href="#">Mujeres</a>
-    <a href="#">Zapatillas</a>
-    <NavBuscador/>
-
-</div>
-  )
+      <Link to="/marcas">Marcas</Link>
+      <Link to="/hombres">Hombres</Link>
+      <Link to="/mujeres">Mujeres</Link>
+      <Link to="/zapatillas">Zapatillas</Link>
+      <NavBuscador />
+    </div>
+  );
 }
 
-export default NavLinks
+export default NavLinks;
