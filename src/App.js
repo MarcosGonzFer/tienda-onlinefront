e4d55ba472e127066cbd7d7b1898bb09457e405e
MarcosGonzFer <marcos0gon@gmail.com>
2024-11-27 19:58:42 +0100

    hecho todos los links en el section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ import Login from './componentes/Login/Login';
 import PerdidaContraseña from './componentes/PerdidaContraseña/PerdidaContraseña';
 import Registro from './componentes/Registro/Registro';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Yeezy from './componentes/Yeezy/Yeezy';
+import Louisvuitton from './componentes/Louisvuitton/Louisvuitton';
+import Nike from './componentes/Nike/Nike';
+import Adidas from './componentes/Adidas/Adidas';
+import NewBalance from './componentes/NewBalance/NewBalance';
+import Jordan from './componentes/Jordan/Jordan';
+import Balenciaga from './componentes/Balenciaga/Balenciaga';
+import Gucci from './componentes/Gucci/Gucci';
+import ListaTiendaColaboraciones from './componentes/ListaTiendaColaboraciones/ListaTiendaColaboraciones';
 
 function App() {
   return (
@@ -14,6 +23,15 @@ function App() {
           <Route path="/Login" element={<Login />} />
           <Route path="/Registro" element={<Registro />} />
           <Route path="/Perdidacontraseña" element={<PerdidaContraseña />} />
+          <Route path="/Louisvuitton" element={<Louisvuitton/>} />
+          <Route path="/Yezzy" element={<Yeezy/>} />
+          <Route path="/Nike" element={<Nike/>} />
+          <Route path="/Adidas" element={<Adidas/>} />
+          <Route path="/NewBalance" element={<NewBalance/>} />
+          <Route path="/Jordan" element={<Jordan/>} />
+          <Route path="/Balenciaga" element={<Balenciaga />} />
+          <Route path="/Gucci" element={<Gucci/>} />
+          <Route path="/ListaTiendaColaboraciones" element={<ListaTiendaColaboraciones/>} />
         </Routes>
       </div>
     </Router>
---
+++ src/componentes/Louisvuitton/Louisvuitton.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../Header/Header'
+import Nav from '../Nav/Nav'
+import Footer from '../Footer/Footer'
+function Louisvuitton() {
+  return (
+    <div>
+    <Header/>
+    <Nav/>
+    Louisvuitton
+    <Footer/>
+</div>
+  )
+}
+
+export default Louisvuitton
