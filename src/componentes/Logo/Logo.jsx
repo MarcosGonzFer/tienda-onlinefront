0277b1f02761cc6bfdc941a16fb2ecf5e4d89bf2
Carlos Humberto Torres Endo <22crls01@gmail.com>
2024-11-28 10:16:48 +0100

    Enlace pagina Home

--- src/componentes/Logo/Logo.jsx
+++ src/componentes/Logo/Logo.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import './Logo.css'
+import "./Logo.css";
+import { Link } from "react-router-dom";
 
-function Logo (){
-    return(
-<div className="logoStyle">
-      <h1>ZAPSHOP</h1>
+function Logo() {
+  return (
+    <div className="logoStyle">
+      <Link to="/" className="linkStyle">
+        <h1>ZAPSHOP</h1>
+      </Link>
     </div>
-
-    );
+  );
 }
-export default Logo
+export default Logo;
