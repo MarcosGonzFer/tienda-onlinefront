d8bde39d91d6cea9767348a0530ff1628c13cd0e
jaime gomez <jagoalius@gmail.com>
2024-12-03 09:47:56 +0100

    hecho hombres, desplegable y mujeres

--- src/componentes/Navlinks/NavLinks.jsx
+++ src/componentes/Navlinks/NavLinks.jsx
@@ -1,35 +1,37 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom'; 
 import './NavLinks.css';
 import NavBuscador from '../NavBuscador/NavBuscador';
 
 function NavLinks() {
   return (
-    <div className="contenedor-nav">
-      {}
+    <nav className="contenedor-nav">
+      
       <div className="nav-item marcas">
-        <a href="#">Marcas</a>
-        
+        <Link to="#" className="nav-link">Marcas</Link>
+
         <div className="dropdown-menu">
-          <a href="/marcas/louis-vuitton">Louis Vuitton</a>
-          <a href="/marcas/nike">Nike</a>
-          <a href="/marcas/gucci">Gucci</a>
-          <a href="/marcas/adidas">Adidas</a>
-          <a href="/marcas/yezze">Yezze</a>
-          <a href="/marcas/Newalance">New Balance</a>
-          <a href="/marcas/Balenciaga">Balenciaga</a>
-          <a href="/marcas/jordan">Jordan</a>
+          <Link to="/Marca/LouisVuitton" className="dropdown-link">Louis Vuitton</Link>
+          <Link to="/Marca/Nike" className="dropdown-link">Nike</Link>
+          <Link to="/Marca/Gucci" className="dropdown-link">Gucci</Link>
+          <Link to="/Marca/Adidas" className="dropdown-link">Adidas</Link>
+          <Link to="/Marca/Yeezy" className="dropdown-link">Yeezy</Link>
+          <Link to="/Marca/NewBalance" className="dropdown-link">New Balance</Link>
+          <Link to="/Marca/Balenciaga" className="dropdown-link">Balenciaga</Link>
+          <Link to="/Marca/Jordan" className="dropdown-link">Jordan</Link>
+          <Link to="/Marca/Dior" className="dropdown-link">Dior</Link>
           
         </div>
       </div>
 
       
-      <a href="/hombres">Hombres</a>
-      <a href="/mujeres">Mujeres</a>
-      <a href="/Accesorios">Accesorios</a>
+      <Link to="/Hombres" className="nav-link">Hombres</Link>
+      <Link to="/Mujeres" className="nav-link">Mujeres</Link>
+      <Link to="/Accesorios" className="nav-link">Accesorios</Link>
 
       
       <NavBuscador />
-    </div>
+    </nav>
   );
 }
 
