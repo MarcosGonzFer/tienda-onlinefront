ffa77bb7157a50798c671a6cb32f7dfe6da2ba1a
MarcosGonzFer <marcos0gon@gmail.com>
2025-01-30 13:11:27 +0100

    cambios para conectar con el back

--- src/componentes/Accesorios/Accesorios.jsx
+++ src/componentes/Accesorios/Accesorios.jsx
@@ -1,88 +1,40 @@
-import React from 'react'
-import Nav from '../Nav/Nav'
-import Header from '../Header/Header'
-import Footer from '../Footer/Footer'
+import React, { useState, useEffect } from 'react'; // Importar useState y useEffect
+import Nav from '../Nav/Nav';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 import { Link } from 'react-router-dom';
 
 function Accesorios() {
-  const accesorios = [
-    {
-      src: '/imagenes/louisvuitton10.jpg',
-      alt: 'Baúl para zapatillas deportivas',
-      label: 'Baúl para zapatillas deportivas',
-      price: '180.000 €',
-    },
-    {
-      src: '/imagenes/yeezy10.png',
-      alt: 'Estuche de maquillaje House',
-      label: 'Estuche de maquillaje House',
-      price: '12.980 €',
-    },
-    {
-      src: '/imagenes/Cordones3.png',
-      alt: 'Cordones',
-      label: 'Solid Gold Cordones',
-      price: '2 Millones €',
-    },
-    {
-      src: '/imagenes/adidas10.png',
-      alt: 'NMD_Rimowa Backpack',
-      label: 'NMD_Rimowa Backpack',
-      price: '10.027 €',
-    },
-    {
-      src: '/imagenes/Cordones2.png',
-      alt: 'Cordones',
-      label: 'Cordones Exclusive: 997G',
-      price: '9.900 €',
-    },
-    {
-      src: '/imagenes/Cordones.png',
-      alt: 'Cordones',
-      label: 'Cordones Off ',
-      price: '12.908 €',
-    },
-    {
-      src: '/imagenes/gucci10.png',
-      alt: 'Microsoft Xbox x Gucci',
-      label: 'Microsoft Xbox x Gucci',
-      price: '21.503 €',
-    },
-    {
-      src: '/imagenes/balenciaga10.png',
-      alt: 'Le Cagole Shoulder Bag',
-      label: 'Le Cagole Shoulder Bag',
-      price: '10.821 €',
-    },
-    {
-      src: '/imagenes/dior10.png',
-      alt: 'Sorayama Android Ring Set ',
-      label: 'Sorayama Android Ring Set ',
-      price: '42.989 €',
-    },
-  ]
+  const [accesorios, setAccesorios] = useState([]); 
+
+  useEffect(() => {
+   
+    fetch('http://localhost:3000/accesorios')
+      .then(response => response.json())
+      .then(data => setAccesorios(data)); 
+  }, []); 
+
   return (
     <div>
       <Header />
       <Nav />
       <div className="lista-zapatillas-colaboraciones">
-      <div className="colaboraciones">
-        <h1>ACCESORIOS</h1>
-      </div>
-      {accesorios.map((acce, index) => (
-        <Link to="/ListaTiendaColaboraciones" key={index} className="caja-zapatillas-colaboraciones">
-          <div>
-            <img src={acce.src} alt={acce.alt} />
-            <h2>{acce.label}</h2>
-            <p className="precio-zapatilla">{acce.price}</p>
-          </div>
-        </Link>
-      ))}
-    </div>
+        <div className="colaboraciones">
+          <h1>ACCESORIOS</h1>
+        </div>
+        <div>
+          {accesorios.map((item, index) => (
+            <Link to="/ListaTiendaColaboraciones" key={index} className="caja-zapatillas-colaboraciones">
+                <img src={item.src} alt={item.alt} />
+                <h2>{item.label}</h2>
+                <p className="precio-zapatilla">{item.price}</p>
+            </Link>
+          ))}
+        </div>
+        </div>
       <Footer />
-
     </div>
-  )
+  );
 }
 
-export default Accesorios
+export default Accesorios;
--- src/componentes/Ofertas/Ofertas.jsx
+++ src/componentes/Ofertas/Ofertas.jsx
@@ -1,91 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../Header/Header";
 import Nav from "../Nav/Nav";
 import Footer from "../Footer/Footer";
 import "./Ofertas.css";
 
 function Ofertas() {
-  const ofertasData = {
-    Ofertas: {
-      title: "Ofertas",
-      className: "ofertas",
-      zapatillas: [
-        {
-          src: "imagenes/pngegg.png",
-          alt: "Air Jordan 1 Retro OG",
-          label: "Air Jordan 1 Retro OG Chicago",
-          originalPrice: "560,00€",
-          discountedPrice: "400,00€",
-        },
-        {
-          src: "imagenes/Balenciaga-Triple-S-All-Over-Logo-Black-White-Womens.png",
-          alt: "Balenciaga",
-          label: "Balenciaga Triple S",
-          originalPrice: "825,00€",
-          discountedPrice: "725,00€",
-        },
-        {
-          src: "imagenes/Nike-Back-To-The-Future-2011.png",
-          alt: "Nike MAG Back to the future",
-          label: "Nike MAG Back to the future",
-          originalPrice: "27871,00€",
-          discountedPrice: "26432,00€",
-        },
-        {
-          src: "imagenes/Gucci-GG-Sneaker-removebg.png",
-          alt: "Gucci GG Sneaker 4",
-          label: "Gucci GG Sneaker 4",
-          originalPrice: "1503,00€",
-          discountedPrice: "1279,00€",
-        },
-        {
-          src: "imagenes/Gucci-GG-Sneaker-removebg.png",
-          alt: "Gucci GG Sneaker 4",
-          label: "Gucci GG Sneaker 4",
-          originalPrice: "1503,00€",
-          discountedPrice: "1279,00€",
-        },
-        {
-          src: "imagenes/Gucci-GG-Sneaker-removebg.png",
-          alt: "Gucci GG Sneaker 4",
-          label: "Gucci GG Sneaker 4",
-          originalPrice: "1503,00€",
-          discountedPrice: "1279,00€",
-        },
-        {
-          src: "imagenes/Gucci-GG-Sneaker-removebg.png",
-          alt: "Gucci GG Sneaker 4",
-          label: "Gucci GG Sneaker 4",
-          originalPrice: "1503,00€",
-          discountedPrice: "1279,00€",
-        },
-        {
-          src: "imagenes/Gucci-GG-Sneaker-removebg.png",
-          alt: "Gucci GG Sneaker 4",
-          label: "Gucci GG Sneaker 4",
-          originalPrice: "1503,00€",
-          discountedPrice: "1279,00€",
-        },
-        {
-          src: "imagenes/Gucci-GG-Sneaker-removebg.png",
-          alt: "Gucci GG Sneaker 4",
-          label: "Gucci GG Sneaker 4",
-          originalPrice: "1503,00€",
-          discountedPrice: "1279,00€",
-        },
-        {
-          src: "imagenes/Balenciaga-Triple-S-All-Over-Logo-Black-White-Womens.png",
-          alt: "Balenciaga",
-          label: "Balenciaga Triple S",
-          originalPrice: "825,00€",
-          discountedPrice: "725,00€",
-        },
-      ],
-    },
-  };
+  const [ofertas, setOfertas] = useState([]); 
 
-  const ofertaSeleccionada = ofertasData.Ofertas;
-  const { title, zapatillas, className } = ofertaSeleccionada;
+  useEffect(() => {
+    fetch('http://localhost:3000/ofertas') 
+      .then(response => response.json())  
+      .then(data => setOfertas(data)) 
+  }, []); 
 
   return (
     <div>
@@ -93,20 +19,11 @@ function Ofertas() {
       <Nav />
       <div className="ofertas-container">
         <div className="ofertas-header">
-          <h1>{title}</h1>
+          <h1>Ofertas</h1> 
         </div>
-        <div
-          className={
-            className ? `lista-zapatillas-${className}` : "lista-zapatillas"
-          }
-        >
-          {zapatillas.map((item, index) => (
-            <div
-              key={index}
-              className={
-                className ? `caja-zapatillas-${className}` : "caja-zapatillas"
-              }
-            >
+        <div className="lista-zapatillas">
+          {ofertas.map((item, index) => (
+            <div key={index} className="caja-zapatillas">
               <img src={item.src} alt={item.alt} />
               <h2>{item.label}</h2>
               <p className="precio-original">{item.originalPrice}</p>
