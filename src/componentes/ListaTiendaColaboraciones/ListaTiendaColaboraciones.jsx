20f03beb2d3bb390d49047482ebcd2bd4ab135a2
MarcosGonzFer <marcos0gon@gmail.com>
2025-02-17 09:39:53 +0100

    colaboraciones hechas

--- src/componentes/ListaTiendaColaboraciones/ListaTiendaColaboraciones.jsx
+++ src/componentes/ListaTiendaColaboraciones/ListaTiendaColaboraciones.jsx
@@ -1,65 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../Header/Header';
 import Nav from '../Nav/Nav';
 import Footer from '../Footer/Footer';
 import './ListaTiendaColaboraciones.css';
 
 function ListaTiendaColaboraciones() {
- const tiendaData = {
-        Tienda: {
-            title: 'Colaboraciones',
-            className: 'colaboraciones',
-            zapatillas: [
-              { src: 'imagenes/nike_offwhite.png', alt: 'Nike x Off-White', label: 'Nike x Off-White', price: '1.847 €'},
-              { src: 'imagenes/adidas_badbunny.png', alt: 'Adidas Campus Bad Bunny', label: 'Adidas Campus Bad Bunny', price: '1.847 €' },
-              { src: 'imagenes/nike_travisscott.png', alt: 'Nike x Travis Scott', label: 'Nike x Travis Scott', price: '1.847 €' },
-              { src: 'imagenes/nike_offwhite.png', alt: 'Nike x Off-White', label: 'Nike x Off-White', price: '1.847 €'},
-              { src: 'imagenes/adidas_badbunny.png', alt: 'Adidas Campus Bad Bunny', label: 'Adidas Campus Bad Bunny', price: '1.847 €' },
-              { src: 'imagenes/nike_travisscott.png', alt: 'Nike x Travis Scott', label: 'Nike x Travis Scott', price: '1.847 €' },
-              { src: 'imagenes/nike_offwhite.png', alt: 'Nike x Off-White', label: 'Nike x Off-White', price: '1.847 €'},
-              { src: 'imagenes/adidas_badbunny.png', alt: 'Adidas Campus Bad Bunny', label: 'Adidas Campus Bad Bunny', price: '1.847 €' },
-              { src: 'imagenes/nike_travisscott.png', alt: 'Nike x Travis Scott', label: 'Nike x Travis Scott', price: '1.847 €' },
-              { src: 'imagenes/nike_offwhite.png', alt: 'Nike x Off-White', label: 'Nike x Off-White', price: '1.847 €'},
-              { src: 'imagenes/adidas_badbunny.png', alt: 'Adidas Campus Bad Bunny', label: 'Adidas Campus Bad Bunny', price: '1.847 €' },
-              { src: 'imagenes/nike_travisscott.png', alt: 'Nike x Travis Scott', label: 'Nike x Travis Scott', price: '1.847 €' },
-              { src: 'imagenes/nike_offwhite.png', alt: 'Nike x Off-White', label: 'Nike x Off-White', price: '1.847 €'},
-              { src: 'imagenes/adidas_badbunny.png', alt: 'Adidas Campus Bad Bunny', label: 'Adidas Campus Bad Bunny', price: '1.847 €' },
-              { src: 'imagenes/nike_travisscott.png', alt: 'Nike x Travis Scott', label: 'Nike x Travis Scott', price: '1.847 €' },
-              { src: 'imagenes/nike_offwhite.png', alt: 'Nike x Off-White', label: 'Nike x Off-White', price: '1.847 €'},
-              { src: 'imagenes/adidas_badbunny.png', alt: 'Adidas Campus Bad Bunny', label: 'Adidas Campus Bad Bunny', price: '1.847 €' },
-              { src: 'imagenes/nike_travisscott.png', alt: 'Nike x Travis Scott', label: 'Nike x Travis Scott', price: '1.847 €' },
-              { src: 'imagenes/nike_offwhite.png', alt: 'Nike x Off-White', label: 'Nike x Off-White', price: '1.847 €'},
-              { src: 'imagenes/adidas_badbunny.png', alt: 'Adidas Campus Bad Bunny', label: 'Adidas Campus Bad Bunny', price: '1.847 €' },
-              { src: 'imagenes/nike_travisscott.png', alt: 'Nike x Travis Scott', label: 'Nike x Travis Scott', price: '1.847 €' },
-             
-            ],
-        },
-    };
+  const [colaboraciones, setColaboraciones] = useState([]);
 
-    const tiendaSeleccionada = tiendaData.Tienda;
-    const { title, zapatillas, className } = tiendaSeleccionada;
+  useEffect(() => {
+    fetch('http://localhost:3000/colaboraciones') 
+      .then(response => response.json())
+      .then(data => setColaboraciones(data))
+      .catch(error => console.error('Error al cargar colaboraciones:', error));
+  }, []);
 
-    return (
-        <div>
-            <Header />
-            <Nav />
-            <div className="colaboraciones-tienda-container">
-                <div className="colaboraciones-tienda-header">
-                    <h1>{title}</h1>
-                </div>
-                <div className={className ? `lista-zapatillas-${className}-tienda` : 'lista-zapatillas'}>
-                    {zapatillas.map((item, index) => (
-                        <div key={index} className={className ? `caja-zapatillas-${className}-tienda` : 'caja-zapatillas'}>
-                            <img src={item.src} alt={item.alt} />
-                            <h2>{item.label}</h2>
-                            <p className="precio">{item.price}</p>
-                        </div>
-                    ))}
-                </div>
-            </div>
-            <Footer />
+  return (
+    <div>
+      <Header />
+      <Nav />
+      <div className="colaboraciones-tienda-container">
+        <div className="colaboraciones-tienda-header">
+          <h1>Colaboraciones</h1> 
         </div>
-    );
+        <div className="lista-zapatillas-tienda">
+          {colaboraciones.map((item, index) => (
+            <div key={index} className="caja-zapatillas-tienda">
+              <img src={item.src} alt={item.alt} />
+              <h2>{item.label}</h2>
+              {item.price && <p className="precio">{item.price}€</p>}
+            </div>
+          ))}
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default ListaTiendaColaboraciones;
